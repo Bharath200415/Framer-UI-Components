3fa1de9119df1df7840d234472e6c727ff0944eb
Bharath200415 <bharath200415@gmail.com>
2026-01-02 16:26:20 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,31 @@ export default function Home() {
               backgroundRepeat:"repeat",
           }}
       className={cn(GeistSans.className,"h-screen flex items-center justify-center mt-2 bg-gray-50")}>
-        {activeComponent === 'Button' ? <Button /> : <div>
+        {activeComponent === 'Button' ? <Button /> : 
+        <div className="relative">
+          <div className="absolute top-20 -left-40 flex items-center gap-2 animate-pulse duration-300">
+            <span className="text-md font-medium text-neutral-600">hover over here</span>
+            <svg 
+              width="40" 
+              height="40" 
+              viewBox="0 0 40 40" 
+              fill="none" 
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-cyan-500 mt-13"
+            >
+              <path 
+                d="M10 10 Q 15 20, 25 25 L 22 27 M 25 25 L 27 22" 
+                stroke="currentColor" 
+                strokeWidth="1.5" 
+                strokeLinecap="round"
+                fill="none"
+              />
+            </svg>
+          </div>
           <Card/>
-          </div>}
+          </div>
+          }
+          
       </div>
     </>
   );
