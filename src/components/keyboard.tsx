57525cc2d5bfbc42784db272010ab25a6a1d4793
Bharath200415 <bharath200415@gmail.com>
2026-01-31 16:46:39 +0530

    cooking

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -4,47 +4,65 @@ import { AnimatePresence, motion } from 'motion/react';
 import React, { useState } from 'react'
 import Image from 'next/image';
 import {Icon24Hours, Icon360, Icon360View, Icon3dCubeSphere, IconMessage, IconPlus, IconX} from '@tabler/icons-react';
+import { TextAnimate } from './ui/text-animate';
 
 export const Keyboard = () => {
     
   return (
-    <div className="max-w-[88rem] mx-auto">
-      <div className="rounded-xl bg-zinc-900 p-4 shadow-2xl">
-        
-        {/* Row 1 */}
-        <div className="grid grid-cols-10 gap-2 mb-2">
-          {["Q","W","E","R","T","Y","U","I","O","P"].map((key) => (
-            <Key key={key} label={key} />
-          ))}
-        </div>
+    <div>
 
-        {/* Row 2 */}
-        <div className="grid grid-cols-9 gap-2 mb-2 ml-4">
-          {["A","S","D","F","G","H","J","K","L"].map((key) => (
-            <Key key={key} label={key} />
-          ))}
-        </div>
 
-        {/* Row 3 */}
-        <div className="grid grid-cols-10 gap-2 mb-2">
-          <Key label="Shift" wide />
-          {["Z","X","C","V","B","N","M"].map((key) => (
-            <Key key={key} label={key} />
-          ))}
-          <Key label="⌫" />
-        </div>
-
-        {/* Space bar */}
-        <div className="grid grid-cols-5 gap-2 mt-3">
-          <Key label="Ctrl" />
-          <Key label="Alt" />
-          <Key label="Space" space />
-          <Key label="Alt" />
-          <Key label="Ctrl" />
-        </div>
-
-      </div>
+    <div className='flex flex-col gap-2'>
+      
+      <TextAnimate 
+      animation="blurInUp" by="character" once
+      className='text-center text-5xl font-semibold font-satoshi text-green-600/60'>Cooking...</TextAnimate>
+      <TextAnimate 
+      animation="blurIn" once
+      className='text-md text-neutral-500 font-medium'>
+        A Cool Component on your way!
+      </TextAnimate>
+  
     </div>
+
+    </div>
+    // <div className="max-w-[88rem] mx-auto"> 
+    //   <div className="rounded-xl bg-zinc-900 p-4 shadow-2xl">
+        
+    //     {/* Row 1 */}
+    //     <div className="grid grid-cols-10 gap-2 mb-2">
+    //       {["Q","W","E","R","T","Y","U","I","O","P"].map((key) => (
+    //         <Key key={key} label={key} />
+    //       ))}
+    //     </div>
+
+    //     {/* Row 2 */}
+    //     <div className="grid grid-cols-9 gap-2 mb-2 ml-4">
+    //       {["A","S","D","F","G","H","J","K","L"].map((key) => (
+    //         <Key key={key} label={key} />
+    //       ))}
+    //     </div>
+
+    //     {/* Row 3 */}
+    //     <div className="grid grid-cols-10 gap-2 mb-2">
+    //       <Key label="Shift" wide />
+    //       {["Z","X","C","V","B","N","M"].map((key) => (
+    //         <Key key={key} label={key} />
+    //       ))}
+    //       <Key label="⌫" />
+    //     </div>
+
+    //     {/* Space bar */}
+    //     <div className="grid grid-cols-5 gap-2 mt-3">
+    //       <Key label="Ctrl" />
+    //       <Key label="Alt" />
+    //       <Key label="Space" space />
+    //       <Key label="Alt" />
+    //       <Key label="Ctrl" />
+    //     </div>
+
+    //   </div>
+    // </div>
   );
 };
 
