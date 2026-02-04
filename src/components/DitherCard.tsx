fa4c6edfb3b8484efd6acac0f882378c7950f887
Bharath200415 <bharath200415@gmail.com>
2026-02-04 20:42:59 +0530

    updates

--- src/components/DitherCard.tsx
+++ src/components/DitherCard.tsx
@@ -12,11 +12,11 @@ export const DitherCard=()=>{
           src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           gridSize={1}
           ditherMode="bayer"
-          colorMode="duotone"
+          colorMode="grayscale"
           primaryColor="#1e3a5f"
           secondaryColor="#f0e68c"
           threshold={0.45}
-          className="h-72 w-[450px] sm:h-100 sm:w-[600px]"
+          className="h-72 w-[450px] sm:h-110 sm:w-[600px]"
         />
       </div>
     </div>
